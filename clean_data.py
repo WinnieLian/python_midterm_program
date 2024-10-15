b56a324754ca3248f4d321b1fd66ac1268aa8f31
NingtaoXu <ningtao1025@gmail.com>
2024-10-15 18:32:50 -0500

    this part is used for looping datasets

--- clean_data.py
+++ clean_data.py
@@ -16,3 +16,27 @@ def clean_data(df):
     df = df.drop_duplicates(subset='title', keep='first')
     
     return df
+
+# Loop through each year, process the data, and combine it
+years = ['2024', '2023', '2022', '2021', '2020',
+         '2019', '2018', '2017', '2016', '2015',
+         '2014', '2013', '2012', '2011', '2010',
+         '2009', '2008', '2007', '2006', '2005',
+         '2004', '2003', '2002', '2001', '2000']
+combined_data = pd.DataFrame()
+
+for year in years:
+    file_path = os.path.join(base_path, f'movies_{year}_filtered.csv')
+    
+    if os.path.exists(file_path):
+        movies = pd.read_csv(file_path, encoding='ISO-8859-1')
+
+        movies.columns = movies.columns.str.strip()
+
+        if 'revenue' in movies.columns:
+            movies_cleaned = clean_data(movies)
+            combined_data = pd.concat([combined_data, movies_cleaned], ignore_index=True)
+        else:
+            print(f"Error")
+    else:
+        print(f"Error")
