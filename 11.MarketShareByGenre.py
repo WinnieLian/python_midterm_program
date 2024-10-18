99054ffadca0c698029475d1e9a3be5964287a24
Chengyang Xu <xuchengyang0207@163.com>
2024-10-17 20:00:15 -0500

    Update 11.MarketShareByGenre.py

--- 11.MarketShareByGenre.py
+++ 11.MarketShareByGenre.py
@@ -8,18 +8,14 @@ non_zero_revenue_data = pd.read_excel(file_path_non_zero_revenue)
 non_zero_revenue_data['release_date'] = pd.to_datetime(non_zero_revenue_data['release_date'], errors='coerce')
 non_zero_revenue_data['release_year'] = non_zero_revenue_data['release_date'].dt.year
 
-# Grouping the data by genre and release year
 genre_year_revenue = non_zero_revenue_data.groupby(['genres', 'release_year'])['revenue'].sum().reset_index()
 
-# Calculating the total revenue per year for all genres
 total_revenue_per_year_genre = genre_year_revenue.groupby('release_year')['revenue'].sum().reset_index()
 total_revenue_per_year_genre = total_revenue_per_year_genre.rename(columns={'revenue': 'total_revenue'})
 
-# Merging the total revenue
 genre_year_revenue = genre_year_revenue.merge(total_revenue_per_year_genre, on='release_year')
 genre_year_revenue['market_share'] = genre_year_revenue['revenue'] / genre_year_revenue['total_revenue']
 
-# Selecting the top 10 genres by total revenue over the entire period
 top_genres = genre_year_revenue.groupby('genres')['revenue'].sum().nlargest(10).index
 top_genre_data = genre_year_revenue[genre_year_revenue['genres'].isin(top_genres)]
 
@@ -29,7 +25,6 @@ other_genre_data = genre_year_revenue[~genre_year_revenue['genres'].isin(top_gen
 other_genre_yearly = other_genre_data.groupby('release_year')['market_share'].sum()
 top_genre_pivot['Other'] = other_genre_yearly
 
-# Define the color
 cmap = mcolors.LinearSegmentedColormap.from_list('teal_green_yellow', ['#4682B4', '#3CB371', '#FFFF66', '#FFE4B5'])
 
 # Plotting
